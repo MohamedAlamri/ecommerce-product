6d407d0ac539b7e438630002b2efea4edfb1f988
MohamedAlamri <99891370+MohamedAlamri@users.noreply.github.com>
2022-09-01 19:04:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -32,20 +32,21 @@ let showData = () => {
     return(
       product1.innerHTML =  DataProduct.map((pro) => {
 
-        let   {item, Title, name, desc, price} = pro
+            let  {item, Title, name, desc, price} = pro
+            let search = productArray.find((x) => x.id === item)
 
             return `
             <h2>${Title}</h2>
                 <h1 class="pro-name">${name}</h1>
                 <p>${desc}</p>
                 <div class="price">
-                    <h3>$ ${price}<span class="offer">50%</span></h3>                      
-                    <h4 class="pre-offer">$ 250</h4>
+                    <h3>$ ${search === undefined || search.count === 0 ? 125 : price * search.count }<span class="offer">50%</span></h3>                      
+                    <h4 class="pre-offer">$ ${search === undefined || search.count === 0 ? 250 : price * search.count * 2}</h4>
                 </div>
                 <div class="add">
                     <div class="minus-plus"  >
                           <img onClick="decrement(${item})" src="images/icon-minus.svg" >
-                          <h4 id=${item} class="count">0</h4>
+                          <h4 id=${item} class="count">${search === undefined ? 0 : search.count}</h4>
                           <img onClick="increment(${item})" src="images/icon-plus.svg" >
                     </div>
                     <a onClick="addToCart(${item})" href="#" class="btn"><img src="images/icon-cart.svg" >Add to cart</a>
@@ -53,6 +54,7 @@ let showData = () => {
             ` 
         })
         )
+
 }
 
 showData()
@@ -118,7 +120,7 @@ let decrement = (item) => {
     else {
         search.count -= 1
     }
-    count.innerHTML = search.count
+    update(item.id)
 
 }
 
@@ -139,7 +141,7 @@ let increment = (item) => {
     }else {
         search.count += 1
     }
-    count.innerHTML = search.count
+    update(item.id)
     localStorage.setItem("Data", JSON.stringify(productArray))
 
 }
@@ -151,18 +153,22 @@ let addToCart = () => {
        
         return(
             productArray.map((y) => {
+                let {count} = y
+                
                 let search = DataProduct.find(x => x.item === y.id);
+                
                 document.querySelector(".shopping-cart").innerHTML = `
                 
+                <h3 class="cart">Cart</h3>
                 <div class="container-cart">
                 <div class="img">
                     <img src="images/pro-1.jpg">
                 </div>
                 <div class="info">
                     <p>${search.name}</p>
-                    <h5 class="price">$ ${search.price}/h5>
+                    <h5 class="price">$ ${search.price} x <span>${count}</span> Total: <span class="total">$ ${search.price * count}</h5>
                 </div>
-                <img onClick="delate(${y.id})" src="images/icon-delete.svg" alt="">
+                <img class="delate" onClick="delate(${y.id})" src="images/icon-delete.svg" alt="">
             </div>
             <a href="#" class="btn">Check out</a>
                 
@@ -172,8 +178,9 @@ let addToCart = () => {
 
     }else{
         document.querySelector(".shopping-cart").innerHTML = `
-            <h4>Your cart is empty</h4>
+            <h4 class="empty">Your cart is empty</h4>
         `
+        document.querySelector(".count").innerText = 0;
     }
        
     }
@@ -182,9 +189,76 @@ let addToCart = () => {
     let delate = (id) => {
         productArray = productArray.filter((x) => x.id !== id.id);
         localStorage.setItem("Data", JSON.stringify(productArray))
-
+        showData()
         addToCart()
     }
 
 
+
+
+
+    let update = (id) => {
+    let search = productArray.find(x => x.id === id)
+        document.getElementById(id).innerText = search.count;
+        addToCart()
+        showData()
+    }
+
+
     addToCart()
+
+
+    // slider for mobile version
+
+    let currentNum = 0;
+    let mobImg = document.querySelectorAll(" .imgs-below img");
+    let btnNext = document.querySelector(".next-btn");
+    let btnPrev = document.querySelector(".prev-btn");
+
+    btnPrev.addEventListener("click", () => {
+        if (currentNum <= 0) {
+            currentNum = mobImg.length ;
+        } 
+
+        currentNum--
+        console.log(currentNum);
+        mobSlid()
+    })
+
+
+    btnNext.addEventListener("click", () => {
+        if (currentNum === mobImg.length - 1) {
+            currentNum = 0
+        }
+
+
+        currentNum++
+        console.log(currentNum, "next");
+        mobSlid()
+    })
+
+
+
+ let   mobSlid = () => {
+        mobImg.forEach((e) => {
+            e.style.transform = `translatex(${currentNum * -100 + "%"})`
+        })
+    }
+
+
+
+
+    // menu for mobile
+    // let mobClose = document.querySelector(".mob-close")
+    // let menuIconMob = document.querySelector(".menu-icon-mob");
+
+    // menuIconMob.addEventListener("click", () => {
+    //     document.querySelector(".links").classList.add("active");
+
+    // })
+
+    // mobClose.addEventListener("click", () => {
+    //     // document.querySelector(".links").classList.remove("active");
+    //     console.log("closed");
+
+    // })
